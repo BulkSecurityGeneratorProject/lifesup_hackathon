92c0295ebb620f0a27a4ee9f760246cfb3dc9e6f
trangtmtran <ttminhtrang09@gmail.com>
2017-09-14 18:43:22 +0300

    created navbar and footer UI

---
+++ src/main/webapp/app/layouts/footer/footer.controller.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('hackathonApp')
+        .controller('FooterController', FooterController);
+
+    FooterController.$inject = ['$scope'];
+
+    function FooterController($scope) {
+        var vm = this;
+        //do something here
+        //
+    }
+})();
