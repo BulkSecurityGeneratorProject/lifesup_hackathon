87ce3e86d4b8669a9b1e440de6ec21064a6710ea
trinhminh <trinhminh1996@gmail.com>
2017-09-21 10:21:43 +0700

    edit ChallengeInfo

--- src/main/java/fi/lifesup/hackathon/web/rest/ChallengeResource.java
+++ src/main/java/fi/lifesup/hackathon/web/rest/ChallengeResource.java
@@ -49,6 +49,10 @@ public class ChallengeResource {
         if (challenge.getId() != null) {
             return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("challenge", "idexists", "A new challenge cannot already have an ID")).body(null);
         }
+        
+        if(challenge.getMaxTeamNumber() >= challenge.getMinTeamNumber()){
+        	return ResponseEntity.badRequest().headers(HeaderUtil.createFailureAlert("challenge", "idexists", "A new challenge cannot already have an ID")).body(null);
+        }
         Challenge result = challengeRepository.save(challenge);
         return ResponseEntity.created(new URI("/api/challenges/" + result.getId()))
             .headers(HeaderUtil.createEntityCreationAlert("challenge", result.getId().toString()))
