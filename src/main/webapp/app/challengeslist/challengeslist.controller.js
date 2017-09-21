bb1375b6e97027926a3e1efabebc611f0a92f750
trangtmtran <ttminhtrang09@gmail.com>
2017-09-21 10:56:55 +0300

    added filter by status and by date

--- src/main/webapp/app/challengeslist/challengeslist.controller.js
+++ src/main/webapp/app/challengeslist/challengeslist.controller.js
@@ -9,19 +9,39 @@
 
     function ChallengesListController($scope, dataservice, $log, $timeout, $q) {
         var vm = this;
-        $scope.Math = Math;
+        vm.Math = Math;
         vm.challenges = [];
         vm.deadline = [];
         vm.getData = getData;
         vm.today = (new Date()).getTime();
-        
+        vm.filterByStatus = filterByStatus;
+        vm.getStatus = getStatus;
+        vm.filter = {};
+
         getData();
 
+        // Functions - Definitions
         function getData() {
             return dataservice.getData()
                 .then(function(data) {
                     vm.challenges = data.challengeslist;
                 });
+        }
+
+        function filterByStatus(challenge) {
+            return vm.filter[challenge.status] || noFilter(vm.filter);
+        }
+            
+        function getStatus() {
+            return (vm.challenges || []).
+              map(function (challenge) { return challenge.status; }).
+              filter(function (cat, idx, arr) { return arr.indexOf(cat) === idx; });
+        }
+
+        function noFilter(filterObj) {
+            return Object.
+              keys(filterObj).
+              every(function (key) { return !filterObj[key]; });
         }   
     }
    
