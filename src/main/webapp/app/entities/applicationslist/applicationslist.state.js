96d3aab9480b2a7604c75dde0dba4bc74d90785f
trangtmtran <ttminhtrang09@gmail.com>
2017-09-27 10:31:01 +0300

    created applications list view, deleted unnecessary lines of code in challengeslist

---
+++ src/main/webapp/app/entities/applicationslist/applicationslist.state.js
@@ -0,0 +1,71 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('hackathonApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider
+            .state('applicationslist', {
+                parent: 'entity',
+                url: '/applicationslist',
+                data: {
+                    authorities: [],
+                    pageTitle: 'hackathonApp.challenge.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'app/entities/applicationslist/applicationslist.html',
+                        controller: 'ApplicationsListController',
+                        controllerAs: 'vm'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('application');
+                        $translatePartialLoader.addPart('applicationslist');
+                        return $translate.refresh();
+                    }]
+                }
+            })
+            // .state('applicationslist-detail', {
+            //     parent: 'entity',
+            //     url: '/applicationslist/{id}',
+            //     data: {
+            //         authorities: [],
+            //         pageTitle: 'hackathonApp.challenge.detail.title'
+            //     },
+            //     views: {
+            //         'content@': {
+            //             templateUrl: 'app/entities/applicationslist/applicationslist-detail.html',
+            //             controller: 'ApplicationsListDetailController',
+            //             controllerAs: 'vm'
+            //         }
+            //     },
+            //     resolve: {
+            //         translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+            //             $translatePartialLoader.addPart('applicationslist');
+            //             $translatePartialLoader.addPart('application');
+            //             return $translate.refresh();
+            //         }],
+            //         entity: ['$stateParams', 'Challenge', function ($stateParams, Challenge) {
+            //             return Challenge.get({ id: $stateParams.id }).$promise;
+            //         }],
+            //         challenge: ['$http', '$stateParams', function($http, $stateParams) {
+            //          $http({
+            //                 url: 'i18n/en/applicationslist.json',
+            //                 method: 'GET',
+            //                 params: {id: $stateParams.id}
+            //             })
+            //                 .then(function(response) {
+            //                     return response.data;
+            //                 })
+            //         }]
+            //     }
+            // })
+    }
+
+})();
--- src/main/webapp/app/entities/challengeslist/challengeslist.service.js
+++ src/main/webapp/app/entities/challengeslist/challengeslist.service.js
@@ -1,29 +1,3 @@
-// angular
-//     .module('hackathonApp')
-//     .factory('dataservice', dataservice);
-
-// dataservice.$inject = ['$http', '$log'];
-
-// function dataservice($http, $log) {
-//     return {
-//         getData: getData
-//     };
-
-//     function getData() {
-//         return $http.get('i18n/en/challengeslist.json')
-//             .then(getDataSuccess)
-//             .catch(getDataFail);
-
-//         function getDataSuccess(response) {
-//             return response.data;
-//         }
-
-//         function getDataFail(error) {
-//             $log.error('ERROR' + error.data);
-//         }
-//     }
-// }
-
 (function () {
     'use strict';
     angular
