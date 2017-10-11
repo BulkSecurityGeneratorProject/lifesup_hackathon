c045deabc81387916939dc2ff84a97a62743d006
Duc Tran <ductranplay@gmail.com>
2017-10-11 16:11:31 +0700

    no message

--- src/main/webapp/app/entities/apply-steps/team.service.js
+++ src/main/webapp/app/entities/apply-steps/team.service.js
@@ -4,13 +4,13 @@
         .module('hackathonApp')
         .factory('InviteMember', InviteMember)
         .factory('AcceptInvitation', AcceptInvitation)
-        // .factory('DeclineInvitation', DeclineInvitation)
+        .factory('ApplicationMembers', ApplicationMembers)
 
 
 
     InviteMember.$inject = ['$resource'];
     AcceptInvitation.$inject = ['$resource'];
-    // DeclineInvitation.$inject = ['$resource'];   
+    ApplicationMembers.$inject = ['$resource'];   
 
     function InviteMember($resource) {
         var resourceUrl = 'api/challenge-user-applications/members';
@@ -48,24 +48,23 @@
         });
     }
 
-    // function DeclineInvitation($resource) {
-    //     var resourceUrl = 'api/challenge-user-applications/:key/invitation';
-
-    //     return $resource(resourceUrl, {}, {
-    //         'query': { method: 'GET', isArray: true },
-    //         'get': {
-    //             method: 'GET',
-    //             transformResponse: function (data) {
-    //                 if (data) {
-    //                     data = angular.fromJson(data);
-    //                 }
-    //                 return data;
-    //             }
-    //         },
-    //         'update': { method: 'PUT' },
-    //         'save': { method: 'POST' }
-    //     });
-    // }
+    function ApplicationMembers($resource) {
+      var resourceUrl =  '/api/challenge-user-applications/member-status/:applicationId';
+    
+      return $resource(resourceUrl, {applicationId: "@applicationId"}, {
+          'query': { method: 'GET', isArray: true},
+          'get': {
+              method: 'GET',
+              transformResponse: function (data) {
+                  if (data) {
+                      data = angular.fromJson(data);
+                  }
+                  return data;
+              }
+          },
+          'update': { method:'PUT' }
+      });
+    }
 
 
 })();
